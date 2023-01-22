ba1e6326a77eec114674f4a26ce22267809f97ea
oiannace <iannaceornello@gmail.com>
2023-01-22 17:26:55 -0500

    made changes to script to run on GCP cloud functions, added requirements.txt

--- attacks/cleverhans/cleverhans_cloud_function.py
+++ attacks/cleverhans/cleverhans_cloud_function.py
@@ -1,10 +1,13 @@
 import os
+import shutil
+import io
 import math
 import json
 import zipfile
 import tempfile
 
-import s3fs
+from google.cloud import storage
+import functions_framework
 
 import numpy as np
 import tensorflow as tf 
@@ -20,33 +23,38 @@ from cleverhans.tf2.attacks.carlini_wagner_l2 import carlini_wagner_l2
 from cleverhans.tf2.attacks.momentum_iterative_method import momentum_iterative_method
 from cleverhans.tf2.attacks.spsa import spsa
 
-def get_s3fs():
-    return s3fs.S3FileSystem(key=os.environ['aws_access_key'], secret=os.environ['aws_access_secret'])
+gcp_project_id = "silken-campus-375517"
+def download_blob(bucket_name, source_blob_name, destination_folder_name):
+    storage_client = storage.Client(project=gcp_project_id)
 
-def s3_unzip_folder(bucket_name: str, folder_name: str):
-  with tempfile.TemporaryDirectory() as tempdir:
-    s3fs = get_s3fs()
-    s3fs.get(f"{bucket_name}/{folder_name}.zip", f"{tempdir}/{folder_name}.zip")
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    data = io.BytesIO(blob.download_as_string())
 
-    with zipfile.ZipFile(f"{tempdir}/{folder_name}.zip") as zip_ref:
-        zip_ref.extractall(f"{tempdir}/{folder_name}")
+    with zipfile.ZipFile(data) as z:
+        z.extractall('/tmp/')
 
-    return tempdir
+@functions_framework.http
+def attack_function(request):
 
-def lambda_handler(event, context):
-
-    bucket_name = "source-bucket"
-    model_name = event['model_name']
-    dataset_name = event['dataset_name']
-
-    tempdir = s3_unzip_folder(bucket_name,model_name)
-    model = keras.models.load_model(f"{tempdir}/{model_name}")
+    request_json = request.get_json(silent=True)
     
-    tempdir = s3_unzip_folder(bucket_name, dataset_name)
+        
+    bucket_name = "capstone-test-bucket"
+    model_name = request_json['model_name']
+    dataset_name = request_json['dataset_name']
     
-    builder = tfds.builder_from_directory(f"{tempdir}/{dataset_name}")
+    download_blob("capstone-test-bucket", model_name, model_name[:-4])
+    model = tf.keras.models.load_model(f"/tmp/{model_name[:-4]}")
+    #remove the model from memory after its loaded to save memory
+    shutil.rmtree(f"/tmp/{model_name[:-4]}", ignore_errors=True)
+    
+    download_blob("capstone-test-bucket", dataset_name, dataset_name[:-4])
+    
+    builder = tfds.builder_from_directory(f"/tmp/{dataset_name[:-4]}")
     test_data = builder.as_dataset(split='test[:2%]', batch_size=128)
-
+    #shutil.rmtree(f"/tmp/{dataset_name[:-4]}", ignore_errors=True)
+    
     test_acc_clean = tf.metrics.SparseCategoricalAccuracy()
     test_acc_fgm = tf.metrics.SparseCategoricalAccuracy()
     test_acc_pgd = tf.metrics.SparseCategoricalAccuracy()
@@ -57,7 +65,7 @@ def lambda_handler(event, context):
     #test_acc_cw = tf.metrics.SparseCategoricalAccuracy()
 
     order_of_norm_lookup = {"inf" : np.inf, "1" : 1, "2" : 2}
-    order_of_norm = order_of_norm_lookup[event['order_of_norm']]
+    order_of_norm = order_of_norm_lookup[request_json['order_of_norm']]
 
     progress_bar_test = tf.keras.utils.Progbar(200)
     for sample in test_data:
@@ -65,41 +73,41 @@ def lambda_handler(event, context):
         x = sample['image']
         y = sample['label']
        
-        if(event['attack_name'] == "None"):
+        if(request_json['attack_name'] == "None"):
             y_pred = model(x)
             test_acc_clean(y, y_pred)
        
-        elif(event['attack_name'] == "FGM"):
-            x_fgm = fast_gradient_method(model_fn = model, x = x, eps = event["epsilon"], norm = order_of_norm)
+        elif(request_json['attack_name'] == "FGM"):
+            x_fgm = fast_gradient_method(model_fn = model, x = x, eps = request_json["epsilon"], norm = order_of_norm)
             y_pred_fgm = model(x_fgm)
             test_acc_fgm(y, y_pred_fgm)
    
-        elif(event['attack_name'] == 'PGD'):
-            x_pgd = projected_gradient_descent(model_fn = model, x = x, eps = event["epsilon"], eps_iter = event["epsilon_iter"], nb_iter = event["attack_iter"], norm = order_of_norm)
+        elif(request_json['attack_name'] == 'PGD'):
+            x_pgd = projected_gradient_descent(model_fn = model, x = x, eps = request_json["epsilon"], eps_iter = request_json["epsilon_iter"], nb_iter = request_json["attack_iter"], norm = order_of_norm)
             y_pred_pgd = model(x_pgd)
             test_acc_pgd(y, y_pred_pgd)
         
-        elif(event['attack_name'] == 'BIM'):
-            x_bim = basic_iterative_method(model_fn = model, x = x, eps = event["epsilon"], eps_iter = event["epsilon_iter"], nb_iter = event["attack_iter"], norm = order_of_norm, rand_init = 0)
+        elif(request_json['attack_name'] == 'BIM'):
+            x_bim = basic_iterative_method(model_fn = model, x = x, eps = request_json["epsilon"], eps_iter = request_json["epsilon_iter"], nb_iter = request_json["attack_iter"], norm = order_of_norm, rand_init = 0)
             y_pred_bim = model(x_bim)
             test_acc_bim(y, y_pred_bim)
 
-        elif(event['attack_name'] == 'MEA'):
-            x_mea = madry_et_al(model_fn = model, x = x, eps = event["epsilon"], eps_iter = event["epsilon_iter"], nb_iter = event["attack_iter"], norm = order_of_norm)
+        elif(request_json['attack_name'] == 'MEA'):
+            x_mea = madry_et_al(model_fn = model, x = x, eps = request_json["epsilon"], eps_iter = request_json["epsilon_iter"], nb_iter = request_json["attack_iter"], norm = order_of_norm)
             y_pred_mea = model(x_mea)
             test_acc_mea(y, y_pred_mea)
 
-        elif(event['attack_name'] == 'MIM'):
-            x_mim = momentum_iterative_method(model_fn = model, x = x, eps = event['epsilon'], eps_iter = event["epsilon_iter"], nb_iter = event["attack_iter"], norm = order_of_norm, decay_factor = event['decay_factor'])
+        elif(request_json['attack_name'] == 'MIM'):
+            x_mim = momentum_iterative_method(model_fn = model, x = x, eps = request_json['epsilon'], eps_iter = request_json["epsilon_iter"], nb_iter = request_json["attack_iter"], norm = order_of_norm, decay_factor = request_json['decay_factor'])
             y_pred_mim = model(x_mim)
             test_acc_mim(y, y_pred_mim)
 
-        #elif(event['attack_name'] == 'SPSA'):
-        #    x_spsa = spsa(model_fn = model, x = x, y = y, eps = event['epsilon'], nb_iter = event["attack_iter"])
+        #elif(request_json['attack_name'] == 'SPSA'):
+        #    x_spsa = spsa(model_fn = model, x = x, y = y, eps = request_json['epsilon'], nb_iter = request_json["attack_iter"])
         #    y_pred_spsa = model(x_spsa)
         #    test_acc_spsa(y, y_pred_spsa)
 
-        #elif(event['attack_name'] == 'CWL2'):
+        #elif(request_json['attack_name'] == 'CWL2'):
         #    x_cw = carlini_wagner_l2(model_fn = model, x = x)
         #    y_pred_cw = model(x_cw)
         #    test_acc_cw(y, y_pred_cw)
@@ -107,24 +115,23 @@ def lambda_handler(event, context):
         progress_bar_test.add(x.shape[0])
        
     
-    if(event['attack_name'] == "None"):
+    if(request_json['attack_name'] == "None"):
         message = "test acc on clean examples (%): {:.3f}".format(test_acc_clean.result() * 100)
-    elif(event['attack_name'] == "FGM"):
+    elif(request_json['attack_name'] == "FGM"):
         message = "test acc on Fast Gradient Method adversarial examples (%): {:.3f}".format(test_acc_fgm.result() * 100)
-    elif(event['attack_name'] == "PGD"):
+    elif(request_json['attack_name'] == "PGD"):
         message = "test acc on Projected Gradient Descent adversarial examples (%): {:.3f}".format(test_acc_pgd.result() * 100)
-    elif(event['attack_name'] == "BIM"):
+    elif(request_json['attack_name'] == "BIM"):
         message = "test acc on Basic Iterative Method adversarial examples (%): {:.3f}".format(test_acc_bim.result() * 100)
-    elif(event['attack_name'] == "MEA"):
+    elif(request_json['attack_name'] == "MEA"):
         message = "test acc on Madry et al adversarial examples (%): {:.3f}".format(test_acc_mea.result() * 100)
-    elif(event['attack_name'] == "MIM"):
+    elif(request_json['attack_name'] == "MIM"):
         message = "test acc on Momentum Iterative Method adversarial examples (%): {:.3f}".format(test_acc_mim.result() * 100)
-    #elif(event['attack_name'] == "SPSA"):
+    #elif(request_json['attack_name'] == "SPSA"):
     #    message = "test acc on SPSA adversarial examples (%): {:.3f}".format(test_acc_spsa.result() * 100)   
-    #else if(event['attack_name'] == "CWL2"):
+    #else if(request_json['attack_name'] == "CWL2"):
     #    message = "test acc on Carlini Wagner L2 adversarial examples (%): {:.3f}".format(test_acc_cw.result() * 100)
     
     return { 
         'result: ' : message
     } 
- 
