0b926fe48dcab3637cb712c49c05e5bf9fe3d12d
Nathan Cheung <nathancheung22@gmail.com>
2023-01-30 12:32:19 -0500

    added navbar/footer for home/contact/attack/about pages

--- src/pages/contact/index.tsx
+++ src/pages/contact/index.tsx
@@ -2,12 +2,10 @@ import Navbar from "@/component/navbar/Navbar";
 import Footer from "@/component/footer/Footer";
 import { Layout } from "antd";
 
-export default function Home() {
+export default function Contact() {
   return (
     <Layout>
       <Navbar />
-
-      
 
       <Footer />
     </Layout>
