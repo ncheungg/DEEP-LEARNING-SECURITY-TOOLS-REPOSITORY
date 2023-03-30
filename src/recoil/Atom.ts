92f7f44beff10bac08f872d10e396277f290f943
Nathan Cheung <nathancheung22@gmail.com>
2023-03-30 19:12:22 -0400

    updated default model/datasets

--- src/recoil/Atom.ts
+++ src/recoil/Atom.ts
@@ -2,13 +2,11 @@ import { atom } from "recoil";
 
 export const modelNameState = atom({
   key: "modelNameState",
-  // default: "test_model",
   default: "",
 });
 
 export const datasetNameState = atom({
   key: "datasetNameState",
-  // default: "cifar10",
   default: "",
 });
 
