cb2697215417fac81906a7bcdf250fb22e0c1e84
Ornello <iannaceornello@gmail.com>
2023-03-30 12:13:34 -0400

    model and dataset type validation

--- src/component/upload/UploadModel.tsx
+++ src/component/upload/UploadModel.tsx
@@ -9,63 +9,37 @@ const { Dragger } = Upload;
 
 
 const UploadModel: React.FC = () => {
-  const [loading, setLoading] = useState(false);
 
   const props: UploadProps = {
-    name: "file",
+    name: "model",
     multiple: false,
-    accept: "zip",
+    accept: ".zip",
     maxCount: 1,
     action: 'https://upload-file-zvax3lpy2q-ue.a.run.app',
 
     onChange(info) {
       //on change occurs anytime the status of the upload changes (file added/removed, upload finished)
-      
-      const {file} = info;
+
+      const { file } = info;
       const status = file.status;
 
-      if(status === "removed"){
+      if (status === "removed") {
         console.log("file remove");
         return;
       }
       //uploading means from users computer to client side
-      if(status == "uploading"){
-        setLoading(true);
+      if (status === "uploading") {
+        console.log("uploading file");
         return;
       }
       if (status === "error") {
-        message.error(`${info.file.name} file upload failed.`);
+        message.error(<>{info.file.name} model upload failed. <br />
+                      See&nbsp;
+                      <a href="https://www.tensorflow.org/guide/saved_model" target="_blank">https://www.tensorflow.org/guide/saved_model</a>
+                      &nbsp;for more information on the TensorFlow SavedModel format.</>, 10);
         return;
       }
       
-      const formData = new FormData();
-      if(file.originFileObj){
-        formData.append('file', file.originFileObj);
-      }
-
-      console.log("uploading file");
-      console.log(formData);
-
-      // fetch('https://dlstr-cleverhans-api-gateway-1brzzfaf.ue.gateway.dev/upload-file', {
-      //   method: 'POST',
-      //   body: formData,
-      // })
-      // .then((response) => {
-      //   if (response.ok) {
-      //     setLoading(false);
-      //     message.success(`${info.file.name} file uploaded successfully.`);
-      //     return response.json();
-      //   }
-      //   throw new Error('Network response was not ok.');
-      // })
-      // .then((data) => {
-      //   console.log('Upload successful');
-      //   console.log(data);
-      // })
-      // .catch((error) => {
-      //   console.error('Error uploading file:', error);
-      // });
-
     },
   };
 
@@ -78,32 +52,15 @@ const UploadModel: React.FC = () => {
         <br />
         <br />
         <br />
-        {/* <InboxOutlined /> */}
-        {/* <CloudSyncOutlined /> */}
-        {/* <CloudUploadOutlined /> */}
         <FileMarkdownOutlined />
       </p>
-      <p className="ant-upload-text">Click or drag to upload keras model here</p>
+      <p className="ant-upload-text">Click or drag to upload model here</p>
       <p className="ant-upload-hint">Compressed (.zip) file containing a Keras model in the Tensorflow SavedModel format</p>
       <br />
       <br />
       <br />
-      
-      {/* <Space direction="vertical" style={{ width: '100%', height: 40 }}>
-        
-        {loading && (
-          <Spin tip="Uploading File...">
-            <div style={{ height: 40 }}></div>
-          </Spin>
-        )}
-        
-      </Space> */}
-      
       <br />
       <br />
-      
-      
-      
       
     </Dragger>
   );
